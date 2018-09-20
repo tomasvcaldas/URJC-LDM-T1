2777f61f6d2528b0300a545b72e51f24ff570977
Maria João Mira Paulo <mariajoaomirapaulo1996@gmail.com>
2018-09-20 16:38:14 +0200

    Exit Game done.

--- app/src/main/java/urjc/ldpquiz/EndGameActivity.java
+++ app/src/main/java/urjc/ldpquiz/EndGameActivity.java
@@ -1,13 +1,17 @@
 package urjc.ldpquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class EndGameActivity extends AppCompatActivity {
 
     private TextView score;
+    private Button menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,17 @@ public class EndGameActivity extends AppCompatActivity {
 
         score = (TextView) findViewById(R.id.score);
         score.setText(Integer.toString(getIntent().getIntExtra("score",0)));
+
+        menu = (Button) findViewById(R.id.menu);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EndGameActivity.this, MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/urjc/ldpquiz/MenuActivity.java
+++ app/src/main/java/urjc/ldpquiz/MenuActivity.java
@@ -12,6 +12,7 @@ public class MenuActivity extends AppCompatActivity {
     private Button capitalsGame;
     private Button flagsGame;
     private Button mixGame;
+    private Button exitGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +22,11 @@ public class MenuActivity extends AppCompatActivity {
         capitalsGame = (Button) findViewById(R.id.capitals);
         flagsGame = (Button) findViewById(R.id.flags);
         mixGame = (Button) findViewById(R.id.mix);
+        exitGame = (Button) findViewById(R.id.exit);
 
         capitalsGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("myTag", "Play Capitals Game");
                 Intent intent = new Intent(MenuActivity.this, CapitalsActivity.class);
                 startActivity(intent);
                 finish();
@@ -35,46 +36,26 @@ public class MenuActivity extends AppCompatActivity {
         flagsGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Log.d("myTag", "Play Flags Game");
                 Intent intent = new Intent(MenuActivity.this, FlagsActivity.class);
                 startActivity(intent);
                 finish();
-
             }
         });
 
         mixGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("myTag", "Play Mix Game");
             }
         });
 
-    }
+        exitGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
 }
